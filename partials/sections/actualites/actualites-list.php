bc21d653071889df93a827d1a7f5dd4f6ba584a9
francoismg <francois.moriergenoud@gmail.com>
2022-06-27 18:07:03 +0200

    Oeuvres - tag cloud + sortable list

---
+++ partials/sections/actualites/actualites-list.php
@@ -0,0 +1,59 @@
+<?php
+
+    $query = new WP_Query(array(
+        'post_type' => 'actualite',
+        'post_status' => 'publish'
+    ));
+
+    $posts = [];
+    $tags = [];
+
+    while ($query->have_posts()) {
+        $query->the_post();
+        $post_id = get_the_ID();
+        $post_fields = get_fields();
+        $post_fields['id'] = get_the_ID();
+        $posts[] = $post_fields;
+        foreach($post_fields['tags'] as $tag) {
+            isset($tags[$tag]) ? $tags[$tag]++ : $tags[$tag] = 1;
+        }
+
+    }
+
+    foreach(array_keys($tags) as $tag) {
+        $tagHydrated = get_tag($tag);
+        $tags[$tag] = ['id' => $tag, 'name' => $tagHydrated->name, 'slug' => $tagHydrated->slug, 'occurences' => $tags[$tag]];
+    }
+
+    usort($tags, function($a, $b) {
+        return $b['occurences'] <=> $a['occurences'];
+    });
+
+    $spanTags = [];
+    $nbTags = sizeof($tags);
+    $i = 0;
+
+    foreach($tags as $tag) {
+
+        $sizeClass = 'bottomSize';
+        $top = 1 - ($i / $nbTags);
+
+        if($top > 0.75) {
+            $sizeClass = 'topSize';
+        } else if($top > 0.5) {
+            $sizeClass = 'middleTopSize';
+        } else if($top > 0.25) {
+            $sizeClass = 'middleBottomSize';
+        }
+
+        $spanTags[] = '<span class="spanTag '.$sizeClass.'" data-id="'.$tag['id'].'">'.$tag['name'].'('.$tag['occurences'].')</span>';
+        $i++;
+    }
+
+    shuffle($spanTags);
+
+    echo '<div class="section__row" style="margin-bottom: 50px !important;">';
+    foreach($spanTags as $spanTag) {
+        echo $spanTag;
+    }
+    echo '</div>';
