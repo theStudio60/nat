87b59e8a4c76cca2a301f7a366f85c2d90fe33e8
Fabien Dupont <fab.d1987@yandex.com>
2022-07-21 16:55:37 +0200

    Update popup.php

--- partials/popup.php
+++ partials/popup.php
@@ -1,12 +1,15 @@
 <div id="popupFrame" class="popup" style="">
-  <div class="container">
+  <div class="container border mt-5 bg-dark">
     <div class="row">
       <div class="col">
-        <div class="content">
-          <div class="popup__header"> 
+        <div class="p-2">
+          <div class="popup__header border-bottom pt-3"> 
+            <h3 class="subtitle subtitle--white">
             <?= the_sub_field('popup_title'); ?>
-            <div class="close">
-              <i  id="" class="fa fa-times" onclick="closePopup();"></i>
+
+            </h3>
+            <div class="close text-white">
+              <i  id="" class="fa fa-times text-white" onclick="closePopup();"></i>
             </div>
           </div>
 
