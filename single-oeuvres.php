531ac307e03556cf7d76ea037378cbbe1ec5387b
Fabien Dupont <fab.d1987@yandex.com>
2022-07-21 17:34:20 +0200

    update

--- single-oeuvres.php
+++ single-oeuvres.php
@@ -20,28 +20,31 @@ endif;
 //*/
 ?>  
 
- <section id=" " class="section border p-4 h-100 w-100 d-block">
-  <div class="container-fluid border p-2 w-100 h-100">
-    <div class="row border p-2 w-100">
-
+ <section id=" " class="section p-0 h-100 w-100 d-block">
  
-     
-  
-            <h1 class="title title--white title--centered "><?php the_title();?></h1>
-  
-            </div>
-            
-              <?php
-              /* Start the Loop */
+  <div class="container-fluid p-0 m-0 w-100 h-100">
+    <div class="row   p-4 w-100">
+      <div class="m-4 w-100 border">
+        <h1 class="title title--white title--centered "><?php the_title();?></h1>
+      </div>
+      <?php
+      /* Start the Loop */
+      while ( have_posts() ) :
+        the_post();
+         get_template_part( 'template-parts/content-oeuvre', get_post_type() );
+       
+       ?>
+      <div class="col-12 col-lg-8 border p-4 m-4">
+        <?php the_content(); ?>
 
-               while ( have_posts() ) :
-                 the_post();
-                 get_template_part( 'template-parts/content-oeuvre', get_post_type() );
-               endwhile;
-             
-              ?>
-             
-  </div>
+      </div>
+<?php    endwhile;
+
+     
+    ?>
+    </div>
+
+  </div> 
 </section>
 <?php
 get_footer();  
--- template-parts/content-oeuvre.php
+++ template-parts/content-oeuvre.php
@@ -17,6 +17,8 @@
 		$desc = get_sub_field('description');
 		$price = get_sub_field('price');
 		$dimensions = get_sub_field('dimensions');
+		$date = get_sub_field('date');
+		$medium = get_sub_field('medium');
 ?>
 	 
 
@@ -25,6 +27,7 @@
 		endwhile;
 	endif;
 ?>   
+<?php /*
 <article  id="post-<?php the_ID(); ?>"
 class="p-2 h-100 w-100 m-0 d-block d-lg-none"
 style=" "
@@ -44,36 +47,116 @@ style=" "
 
 </article><!-- #post-<?php the_ID(); ?> -->
 
-
+//*/ ?>
 
 
 <article  id="post-<?php the_ID(); ?>"
-class="p-2  w-100 m-0 d-none d-lg-block"
+class=" w-100 border  m-4  p-4 "
 style=" "
->
+> 
 	<div class="row h-100 d-flex p-0 m-0">
-		<div class="w-75 d-flex " >
+		<div class="col-12 col-lg-8 d-flex  " >
 
-			<img style="overflow-y:scroll;max-width:640px "  class="img-fluid d-block m-auto border" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+			<img style="overflow-y:scroll; max-height:480px; width:auto; "  class="img-fluid d-block d-lg-none p-4 m-auto" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+			<img style="overflow-y:scroll; max-height:640px "  class="img-fluid d-none d-lg-block m-auto" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
 	
 	 	</div>
 
-		<div class="w-25 p-2 border text-white">
+		<div class="col-12 col-lg-4 p-4 border text-white  ">
 			<?php // the_content(); ?>
-			<?= $desc ?>
-			<?= $price ?>
-			<?= $dimensions ?>
+			<div class="d-block d-lg-none">
+
+					<div class="mt-2">
+		
+						<p class="§ §--white pt-0 mb-4">
+							<?= $desc ?>	
+						<p> 
+			
+
+						<p class="§ §--white pt-0 my-4">
+							Taille : <?= $dimensions ?>	
+						<p> 
+		
+		
+						<p class="§ §--white pt-0 my-4">
+							Médium : <?= $medium ?>
+						<p>
+
+						<p class="§ §--white pt-0 my-4">
+						Date de création : <?= $dare ?>
+						<p>
+						<p class="§ §--white pt-0 my-4">
+						Prix : <?= $price ?>
+		
+						<p>
+					</div>
+
+					<div class="mt-auto mb-0 p-4">
+						<div class="w-100 d-flex justify-content-around">
+
+							<button class="button--sucre">
+        	      <span>
+        	     	+ Sucre
+
+        	      </span>
+        	    </button>
+        	    <button class="button--sel">
+        	      <span>
+        	      - Sel
+
+        	      </span>
+        	    </button>
+						</div>
+					</div>
+			</div>
+			<div class="d-none d-lg-flex flex-column h-100">
+
+					<div class="mt-2">
+		
+						<p class="§ §--white pt-0 mb-4">
+							<?= $desc ?>	
+						<p> 
+			
+
+						<p class="§ §--white pt-0 my-4">
+							Taille : <?= $dimensions ?>	
+						<p> 
+		
+		
+						<p class="§ §--white pt-0 my-4">
+							Médium : <?= $medium ?>
+						<p>
+
+						<p class="§ §--white pt-0 my-4">
+						Date de création : <?= $dare ?>
+						<p>
+						<p class="§ §--white pt-0 my-4">
+						Prix : <?= $price ?>
+		
+						<p>
+					</div>
+
+					<div class="mt-auto mb-0 p-4">
+						<div class="w-100 d-flex justify-content-around">
+
+							<button class="button--sucre">
+        	      <span>
+        	     	+ Sucre
+
+        	      </span>
+        	    </button>
+        	    <button class="button--sel">
+        	      <span>
+        	      - Sel
+
+        	      </span>
+        	    </button>
+						</div>
+					</div>
+			</div>
 		</div>
 
-
-
 	</div>
-
-
-
-
 </article><!-- #post-<?php the_ID(); ?> -->
 
-<div class="col-9">
- 	<?php the_content(); ?>
- </div>
+ 
