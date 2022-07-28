8acb8c8f5bd2d1ff35f9bae93e87b792f735c924
Fabien Dupont <fab.d1987@yandex.com>
2022-07-28 22:35:04 +0200

    ajout deuxième système accordion

--- template-parts/content-projet.php
+++ template-parts/content-projet.php
@@ -46,7 +46,7 @@ style=" "
        
           <?php $boxID = get_sub_field('jsID'); ?>
  
-              <?php // endwhile; ?>
+          <?php // endwhile; ?>
           <div id="accordion<?= $boxID ?>" class="w-100" >
             <div class="c" id="heading<?= $boxID ?>">
               <h5 class="mb-0">
@@ -57,6 +57,11 @@ style=" "
             </div>
             <div id="collapse<?= $boxID ?>" class="collapse " aria-labelledby="heading<?= $boxID ?>" data-parent="#accordion<?= $boxID ?>">
               <div class="§ §--white text-white mt-1 mb-5">
+                <?= $content ?>  
+                <?php if ($image) : ?>
+                  <img class="img-fluid border" src="<?php echo $image[url] ?>" alt="">
+                <?php endif; ?>
+                
                 <?php if( have_rows('flex-item') ): ?>
                   <?php while( have_rows('flex-item') ): the_row(); ?>
                     <?php  if( get_row_layout() == 'image') : ?>
@@ -68,7 +73,7 @@ style=" "
  
                           <img class="d-none d-lg-block d-xl-none border" width="664" src="<?= $img['url'] ?>"/> 
 
-                          <img class="d-none d-md-block d-lg-none border" width="543" src="<?= $img['url'] ?>"/> 
+                          <img class="d-none d-md-block d-lg-none border" width="543" src ="<?= $img['url'] ?>"/> 
 
                           <img class="d-none d-sm-block d-md-none border" width="478" src="<?= $img['url'] ?>"/> 
 
@@ -108,7 +113,7 @@ style=" "
                           <video class="d-none d-sm-block d-md-none" width="478" height="auto" controls>
                             <source src="<?= $mp4['url'] ?>" type="video/mp4"> 
                           </video> 
-                          <video class="d-block d-sm-none mx-auto" width="288" height="auto" onclick="location.href='<?php the_permalink(); ?>'"  >
+                          <video class="d-block d-sm-none mx-auto" width="288" height="auto" controls>
                             <source src="<?= $mp4['url'] ?>" type="video/mp4"> 
                           </video> 
                         </div>
@@ -116,14 +121,122 @@ style=" "
                     <?php endif;?>
                   <?php endwhile; ?>
                 <?php endif;?>
-                <?= $content ?>  
-                <?php if ($image) : ?>
-                  <img class="img-thumbnail" src="<?php echo $image[url] ?>" alt="">
-                <?php endif; ?>
+
               </div>
             </div>
           </div>
         <?php endif;?>
+
+       
+        <?php  if( get_row_layout() == 'box-accordion-image_flex') : ?>
+          <?php  //while( have_rows('box-accordion_flex') ): the_row(); ?>
+
+          <?php $title = get_sub_field('title'); ?>
+          <?php $titleImage = get_sub_field('title-image'); ?>
+          <?php $content = get_sub_field('wysiwyg'); ?>
+          <?php $image = get_sub_field('main-image'); ?>
+       
+          <?php $boxID = get_sub_field('jsID'); ?>
+ 
+          <?php // endwhile; ?>
+          <div id="accordion<?= $boxID ?>" class="w-100" >
+            <div class="c" id="heading<?= $boxID ?>">
+              <h5 class="mb-0">
+                <button class="button button--fluid p-4 my-2" data-toggle="collapse" data-target="#collapse<?= $boxID ?>" aria-expanded="false" aria-controls="collapse<?= $boxID ?>">
+                  <?= $title ?>  <?php //  echo  the_ID();?>
+                  <?php if ($titleImage ) :?>
+                        <div class="py-4">
+
+                          <img  class="d-none d-xl-block mx-auto" style="max-width:480px;"  src="<?= $titleImage['url'] ?>"> 
+ 
+                          <img class="d-none d-lg-block d-xl-none mx-auto" style="max-width:400px;" src="<?= $titleImage['url'] ?>"/> 
+
+                          <img class="d-none d-md-block d-lg-none mx-auto" style="max-width:320px;" src="<?= $titleImage['url'] ?>"/> 
+
+                          <img class="d-none d-sm-block d-md-none mx-auto" style="max-width:256px;" src="<?= $titleImage['url'] ?>"/> 
+
+                          <img class="d-block d-sm-none mx-auto" style="max-width:240px;" src="<?= $titleImage['url'] ?>" /> 
+ 
+                        </div>
+                        <?php // var_dump($img) ; ?>
+                  <?php endif;?>
+                </button>
+              </h5>
+            </div>
+            <div id="collapse<?= $boxID ?>" class="collapse " aria-labelledby="heading<?= $boxID ?>" data-parent="#accordion<?= $boxID ?>">
+              <div class="§ §--white text-white mt-1 mb-5">
+                <?= $content ?>  
+                <?php if ($image) : ?>
+                  <img class="img-fluid border" src="<?php echo $image[url] ?>" alt="">
+                <?php endif; ?>
+                
+                <?php if( have_rows('flex-item') ): ?>
+                  <?php while( have_rows('flex-item') ): the_row(); ?>
+                    <?php  if( get_row_layout() == 'image') : ?>
+                      <?php $img = get_sub_field('image');  ?>
+                      <?php if ($img ) :?>
+                        <div class="py-4">
+
+                          <img  class="d-none d-xl-block border" width="708" src="<?= $img['url'] ?>"> 
+ 
+                          <img class="d-none d-lg-block d-xl-none border" width="664" src="<?= $img['url'] ?>"/> 
+
+                          <img class="d-none d-md-block d-lg-none border" width="543" src ="<?= $img['url'] ?>"/> 
+
+                          <img class="d-none d-sm-block d-md-none border" width="478" src="<?= $img['url'] ?>"/> 
+
+                          <img class="d-block d-sm-none mx-auto" width="288" height="auto" src="<?= $img['url'] ?>" /> 
+ 
+                        </div>
+                        <?php // var_dump($img) ; ?>
+                      <?php endif;?>
+                    <?php endif;?>
+
+                    <?php  if( get_row_layout() == 'wysiwyg') :?>
+                      <?php $thisContent = get_sub_field('wysiwyg'); ?>
+                      <?= $thisContent ?>
+                    <?php endif;?>
+
+                    <?php  if( get_row_layout() == 'audio') :?>
+                      <?php $thisFile = get_sub_field('audio'); ?>
+                      <audio id="audio<?php echo the_ID() ?>"  class="d-block w-100 my-2 border audio-player" controls >
+                        <source src="<?= $thisFile['url'] ?>" type="audio/mpeg "> 
+                      </audio>
+              
+                    <?php endif;?>
+
+                    <?php  if( get_row_layout() == 'video') : ?>
+                      <?php $mp4 = get_sub_field('video'); ?>
+                      <?php  if ($mp4) :?> 
+                        <div class="py-4">
+                          <video  class="d-none d-xl-block" width="708" height="auto" controls>
+                            <source src="<?= $mp4['url'] ?>" type="video/mp4"> 
+                          </video> 
+                          <video class="d-none d-lg-block d-xl-none" width="664" height="auto" controls>
+                            <source src="<?= $mp4['url'] ?>" type="video/mp4"> 
+                          </video> 
+                          <video class="d-none d-md-block d-lg-none" width="543" height="auto" controls>
+                            <source src="<?= $mp4['url'] ?>" type="video/mp4"> 
+                          </video> 
+                          <video class="d-none d-sm-block d-md-none" width="478" height="auto" controls>
+                            <source src="<?= $mp4['url'] ?>" type="video/mp4"> 
+                          </video> 
+                          <video class="d-block d-sm-none mx-auto" width="288" height="auto" controls>
+                            <source src="<?= $mp4['url'] ?>" type="video/mp4"> 
+                          </video> 
+                        </div>
+                      <?php endif; ?> 
+                    <?php endif;?>
+                  <?php endwhile; ?>
+                <?php endif;?>
+
+              </div>
+            </div>
+          </div>
+        <?php endif;?>
+
+
+
       <?php endwhile; ?>
     <?php endif;?>
   <?php endwhile;?>
