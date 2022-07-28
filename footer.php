799a2c943b63ef05f1876cc55c4daf02b81a7beb
Fabien Dupont <fab.d1987@yandex.com>
2022-07-28 22:21:40 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,15 +4,16 @@
     <div class="footer__row d-flex ">
       <div class="footer__bottom ">
         <span style="font-size:12px">
-          © <?= date('Y');?> - <?= get_bloginfo(); ?> - 
+          © <?= date('Y');?> - <?= get_bloginfo(); ?> 
         </span> 
         <span class="mx-2" style="font-size:12px">
-           <?php printf( esc_html__( 'Conception & Développement : %2$s', 'studio60' ), 'studio60', '<a class="text-uppercase" href="http://www.studio60.ch">STUDIO60</a>' );?>
+           <?php // printf( esc_html__( 'Conception & Développement : %2$s', 'studio60' ), 'studio60', '<a class="text-uppercase" href="http://www.studio60.ch">STUDIO60</a>' );?>
         </span> 
+        <?php /*
         <span style="font-size:12px">
           <a class=""href="<?= get_home_url() ?>/login"><i class="text-white fa fa-lock"></i></a>
         </span>
-
+        //*/ ?>
       </div>
     </div><!-- end row -->
 
